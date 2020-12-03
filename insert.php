e8c4b5376aebc9b2d393a7baebe7ea288d787f2c
jiuncheng <46194899+jiuncheng@users.noreply.github.com>
2020-12-03 12:48:49 +0800

    Added new file

---
+++ insert.php
@@ -0,0 +1,20 @@
+<?php
+$connectionInfo = array("UID" => "simplewebadmin", "pwd" => "Admin123", "Database" => "simpleweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:simplewebtp046253.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if (!$conn) {
+    die("Error connection: " . sqlsrv_errors());
+} else {
+    echo "<script>alert('success!');</script>";
+}
+
+$sql = "INSERT INTO restaurant (restaurant_name, restaurant_address, restaurant_phone) VALUES (?, ?, ?);";
+$params = array("7 eleven", "Bukit Jalil", "03-82727626");
+
+if ($results == FALSE) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+echo "<script>alert('Data inserted.');</script>";
+echo "<script>window.location.replace = 'viewtable.php';</script>";
