ad40de7ea15242159c64e758c8b050352b2d8940
jiuncheng <46194899+jiuncheng@users.noreply.github.com>
2020-12-03 12:31:21 +0800

    Added database connection

--- viewtable.php
+++ viewtable.php
@@ -1,31 +1,49 @@
 <html>
-  <head>
+
+<head>
     <title>List of KL Restaurant</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <center>
-      <h1>List of KL Restaurant</h1>
-      <!--create table structure using HTML first-->
-      <table border="1">
-        <tr>
-          <th>Restaurant ID</th>
-          <th>Restaurant Name</th>
-          <th>Address</th>
-          <th>Phone</th>
-        </tr>
-        <tr>
-          <th>0</th>
-          <th>Subway Restaurant</th>
-          <th>Menara Standard Chartered, TPM</th>
-          <th>03-22441234</th>
-        </tr>
-        <tr>
-          <th>1</th>
-          <th>Food Restaurant</th>
-          <th>Menara Standard Chartered, TPM</th>
-          <th>03-67331234</th>
-        </tr>
-      </table>
+        <h1>List of KL Restaurant</h1>
+        <!--create table structure using HTML first-->
+        <table border="1">
+            <tr>
+                <th>Restaurant ID</th>
+                <th>Restaurant Name</th>
+                <th>Address</th>
+                <th>Phone</th>
+            </tr>
+            <tr>
+                <th>0</th>
+                <th>Subway Restaurant</th>
+                <th>Menara Standard Chartered, TPM</th>
+                <th>03-22441234</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <th>Food Restaurant</th>
+                <th>Menara Standard Chartered, TPM</th>
+                <th>03-67331234</th>
+            </tr>
+
+            <?php
+            $connectionInfo = array("UID" => "simplewebadmin", "pwd" => "{your_password_here}", "Database" => "simpleweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+            $serverName = "tcp:simplewebtp046253.database.windows.net,1433";
+            $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+            if (!$conn) {
+                die("Error connection: " . sqlsrv_errors());
+            } else {
+                echo "success!";
+            }
+
+
+            ?>
+
+        </table>
     </center>
-  </body>
+</body>
+
 </html>
